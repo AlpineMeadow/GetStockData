f3fb95a2f9710f9a3179ffc45169024678a763f8
jdw <jdw.space@gmail.com>
2020-05-25 13:11:13 -0600

    Adding GetStockData29.py

---
+++ GetStockData29.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+#A program to get stock data.
+
+#Gather our code in a main() function.
+def main() :
+    import sys
+    sys.path.append('/home/jdw/UM2020Spring/M567/Functions/')
+    import StockFunctions as sf
+
+
+    #First we get some data.  This function returns an array of times and prices.
+    #First set the length of time to get.
+    numHours = 6
+    numSec = numHours * 360
+    sleepTime = 10
+
+    #The name of the stock we are looking at.
+    tag = 'vz'  #Verizon
+    data = sf.query(numSec, sleepTime, tag)
+
+    
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+  main()
