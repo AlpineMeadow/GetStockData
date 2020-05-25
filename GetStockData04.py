eea5e60b2276aa45f52171f328a50d352f491904
jdw <jdw.space@gmail.com>
2020-05-25 17:43:04 -0600

    Added plotting

--- GetStockData04.py
+++ GetStockData04.py
@@ -5,21 +5,40 @@
 #Gather our code in a main() function.
 def main() :
     import sys
-    sys.path.append('/home/jdw/UM2020Spring/M567/Functions/')
-    import StockFunctions as sf
+    import time
+    sys.path.append('/home/jdw/UM2020Spring/M567/GetStockFunctions/')
+    import getStockFunctions as gsf
 
 
     #First we get some data.  This function returns an array of times and prices.
     #First set the length of time to get.
     numHours = 6
     numSec = numHours * 360
+
+    #The variable sleepTime is the number of seconds to sleep before getting
+    #another stock price.
     sleepTime = 10
 
+    #Let us get the data and time.
+    dt = time.localtime()
+    yearStr = str('{0:02d}'.format(dt.tm_year))
+    monthStr = str('{0:02d}'.format(dt.tm_mon))
+    dayStr = str('{0:02d}'.format(dt.tm_mday))
+    hourStr = str('{0:02d}'.format(dt.tm_hour))
+    minuteStr = str('{0:02d}'.format(dt.tm_min))
+
+    dateTime = (yearStr + monthStr + dayStr + hourStr + minuteStr)
+
+    
     #The name of the stock we are looking at.
-    tag = 'amzn'  #Amazon
-    data = sf.query(numSec, sleepTime, tag)
+    stockAbbrev = 'axp'  #AmericanExpress
+    
+    #Now get the data from the IEX server.
+    data = gsf.query(numSec, sleepTime, stockAbbrev, dateTime)
 
-
+    #Now plot the data.
+    succeed = gsf.visualizeDailyStockPrice(data, stockAbbrev, dateTime)
+  
     
 # Standard boilerplate to call the main() function to begin
 # the program.
